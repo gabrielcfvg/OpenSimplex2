89929a51ef42a3b1f420adb7ec5363b58f2ad579
Rafał Mikrut <mikrutrafal@protonmail.com>
2025-11-11 18:57:36 +0100

    Make std optional, but disabled by default

--- examples/seed_2d.rs
+++ examples/seed_2d.rs
@@ -7,15 +7,16 @@ fn main() {
 
     for _ in 0..10 {
         let iteration_time = Instant::now();
+        let mut res = 0.0; // To not optimize away the loop
 
         for x in 0..8000 {
             for y in 0..8000 {
-                let _ = opensimplex2::fast::noise2(SEED, x as f64, y as f64);
+                res += opensimplex2::fast::noise2(SEED, x as f64, y as f64);
             }
         }
 
         println!(
-            "Rust Impl 2D: {} msec",
+            "Rust Impl 2D: {} msec (Res: {res})",
             iteration_time.elapsed().as_millis()
         );
     }
